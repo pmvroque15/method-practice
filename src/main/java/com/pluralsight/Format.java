46350f5833e626de94fe66789f605d11b5f2d2f2
pmvroque15 <pmvroque@gmail.com>
2026-04-10 12:08:30 -0400

    Format.java file initialized

---
+++ src/main/java/com/pluralsight/Format.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Format {
+    static void main() {
+        //        4. 5. 1. 2. Create a new file named Format.java
+            //        a. Create a main() method
+            //        a. Create this method: formatName(String first,
+            //                String last)
+            //        b. It should RETURN "Last, First" format
+            //        b. Example: formatName("Bob", "Smith") returns "Smith, Bob"
+            //        c. Call it in your main and put it in the () of a System.out.println(). Bonus:
+            //        set the return of the method call in a variable and print that!
+    }
+}
--- src/main/java/com/pluralsight/Menu.java
+++ src/main/java/com/pluralsight/Menu.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 
 public class Menu {
 
-//        4. 5. 1. 2. Create a new file named Format.java
-//        a. Create a main() method
-//        a. Create this method: formatName(String first,
-//                String last)
-//        b. It should RETURN "Last, First" format
-//        b. Example: formatName("Bob", "Smith") returns "Smith, Bob"
-//        c. Call it in your main and put it in the () of a System.out.println(). Bonus:
-//        set the return of the method call in a variable and print that!
 //        a. Create main() method
     static void main(String[] args) {
         //Initialize Scanner
