9aaddc0d0d6fd0320009d95cd99109645b08044e
pmvroque15 <pmvroque@gmail.com>
2026-04-10 12:24:15 -0400

    Format.java file is complete

--- src/main/java/com/pluralsight/Format.java
+++ src/main/java/com/pluralsight/Format.java
@@ -1,14 +1,19 @@
 package com.pluralsight;
-
+// Create a new file named Format.java
 public class Format {
-    static void main() {
-        //        4. 5. 1. 2. Create a new file named Format.java
-            //        a. Create a main() method
-            //        a. Create this method: formatName(String first,
-            //                String last)
-            //        b. It should RETURN "Last, First" format
-            //        b. Example: formatName("Bob", "Smith") returns "Smith, Bob"
-            //        c. Call it in your main and put it in the () of a System.out.println(). Bonus:
-            //        set the return of the method call in a variable and print that!
+    // Create a main() method
+    static void main(String[] args) {
+        System.out.print(formatName("Pat","Roque"));
     }
+    // Create this method: formatName(String first,String last)
+    static String formatName(String first,String last) {
+        // It should RETURN "Last, First" format
+        // Example: formatName("Bob", "Smith") returns "Smith, Bob"
+        // Call it in your main and put it in the () of a System.out.println()
+        // Bonus: set the return of the method call in a variable and print that!
+        return first + " " + last;
+    }
+
 }
+
+
