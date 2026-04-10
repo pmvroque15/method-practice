b7c4179603579cb4a4175b8154bcb129497d59a3
pmvroque15 <pmvroque@gmail.com>
2026-04-10 11:11:12 -0400

    added sayGoodbye method

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -4,17 +4,23 @@ import java.util.Scanner;
 
 public class Hello {
 
-//            c. sayGoodbye() that prints "Goodbye!"
+
 //            d. sayGoodMorning() that prints "Good morning!"
 //    e. Call each method from main()
     // Creating main() method
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         sayHello(scanner);
+        sayGoodbye(scanner);
     }
     // sayHello() that prints "Hello, World!"
     static void sayHello(Scanner scanner) {
         System.out.print("Type \"Hello World!\": ");
         String userHelloWorld = scanner.nextLine();
+    }
+    // sayGoodbye() that prints "Goodbye!"
+    static void sayGoodbye(Scanner scanner) {
+        System.out.print("Type \"Goodbye!\": ");
+        String userGoodbye = scanner.nextLine();
     }
 }
