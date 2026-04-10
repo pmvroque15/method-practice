d40e7ea5673c8c359d68bf08788ed1d676a1422d
pmvroque15 <pmvroque@gmail.com>
2026-04-10 11:19:53 -0400

    finished exercise 1,next exercise 2

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -7,24 +7,30 @@ public class Hello {
         // Call each method from main()
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         sayHello(scanner);
         sayGoodbye(scanner);
+        sayGoodMorning(scanner);
     }
     // sayHello() that prints "Hello, World!"
     static void sayHello(Scanner scanner) {
         System.out.print("Type \"Hello World!\": ");
         String userHelloWorld = scanner.nextLine();
 
-        System.out.print("You typed: " + userHelloWorld);
+        System.out.println("You typed: " +userHelloWorld);
     }
     // sayGoodbye() that prints "Goodbye!"
     static void sayGoodbye(Scanner scanner) {
         System.out.print("Type \"Goodbye!\": ");
         String userGoodbye = scanner.nextLine();
+
+        System.out.println("You typed: " + userGoodbye);
     }
     // sayGoodMorning() that prints "Good morning!"
     static void sayGoodMorning(Scanner scanner) {
         System.out.print("Type \"Good morning\": ");
         String userGoodMorning = scanner.nextLine();
+
+        System.out.println("You typed: " + userGoodMorning);
     }
 }
