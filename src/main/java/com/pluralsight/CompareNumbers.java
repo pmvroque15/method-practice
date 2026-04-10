10b038a154d6459a342f513d84cf8e97ae2b7e6d
pmvroque15 <pmvroque@gmail.com>
2026-04-10 13:04:57 -0400

    CompareNumbers file done

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
 public class CompareNumbers {
+//    a. Create a main() method
+    static void main(String[] args) {
+        //e. Call these within a sout to view if your methods work or not. EX.
+            // System.out.println(isEven(5)); should display false
+        System.out.println(isEven(7));
+        System.out.print(isPositive(-7));
+    }
+    // b. Create this method: isEven(int number)
+    static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            //    a. returns true if even
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //    c. Create: isPositive(double number)
+        //    d. returns true if > 0
+    static boolean isPositive(double number) {
+        if (number > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
